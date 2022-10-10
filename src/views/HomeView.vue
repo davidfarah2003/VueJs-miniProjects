a883454e884ee63f84aec0d7b03c401a39c49878
davidf <david.farah@epfl.ch>
2022-10-10 22:33:33 +0200

    fetch data with json-server

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <div v-if="error">{{error}}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts"/>
+    </div>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue'
+import PostList from '../components/PostList.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  name: 'Home',
+  components: { PostList },
+  setup() {
+    const posts = ref([])
+    const error = ref(null)
+
+    const load = async () => {
+      try {
+        let data = await fetch('http://localhost:3000/posts')
+        if (!data.ok){
+          throw Error('no data available')
+        }
+        posts.value = await data.json()
+
+      } catch (err) {
+        error.value = err.message
+        console.log(error.value)
+      }
+    }
+
+    load()
+    return { posts, error }
+  },
 }
 </script>
