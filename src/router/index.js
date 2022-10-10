f24769119ffe9d08cda5d1e26bd3d86df2b173f2
davidf <david.farah@epfl.ch>
2022-10-10 22:56:32 +0200

    Add Details page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Details from "@/views/Details";
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: HomeView
+  },
+  {
+    path: '/posts/:id',
+    name: 'Details',
+    component: Details,
+    props: true //if we link to this page we can use the route param (id) as a prop
   },
 ]
 
